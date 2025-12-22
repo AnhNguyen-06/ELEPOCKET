be6080c45220523e1d219ba8abbcaf6dea97363f
Anh Nguyen <731819nguyenvanquanganh@gmail.com>
2025-12-22 14:27:48 +0700

    update lang

--- lang.js
+++ lang.js
@@ -1,5 +1,6 @@
 const translations = {
     en: {
+        // Navigation bar
         "nav-home": "Home",
         "nav-about": "About us",
         "nav-quicktest": "Quick Test",
@@ -7,14 +8,44 @@ const translations = {
         "nav-signup": "Sign up",
         "search-placeholder": "Search...",
         
+        // Home
+
+        // About us
+        "page-aboutus-title": "About us",
+
+        // Quick test
+
+        // Blog
         "page-blog-title": "Blog",
         "blog-1-title": "What even is ELEPOCKET?",
         "blog-1-desc": "The first ELEBlog ever!",
 
         "contact-label": "Contact",
-        "footer-info-title": "Contact info"
+        "footer-info-title": "Contact info",
+
+        // Sign up - Sign in
+        "sign-up-title": "Sign up",
+        "sign-in-title": "Sign in",
+        "username-placeholder": "Username",
+        "email-placeholder": "Email",
+        "password-placeholder": "Password",
+        "agree": "I agree to the",
+        "terms-of-service": "Terms of Service",
+        "and": "and",
+        "privacy-policy": "Privacy Policy",
+        "of-ELE": "of Elepocket",
+        "sign-up-button": "Sign up",
+        "sign-in-button": "Sign in",
+        "login-now": "Login now",
+        "signup-now": "Register now",
+        "already-have-account": "Already have an account?",
+
+        "remember": "Remember me",
+        "forgot-password": "Forgot password?",
+        "dont-have-account": "Don't have an account?",
     },
     vi: {
+        // Thanh điều hướng
         "nav-home": "Trang chủ",
         "nav-about": "Về chúng tôi",
         "nav-quicktest": "Test nhanh",
@@ -22,12 +53,39 @@ const translations = {
         "nav-signup": "Đăng ký",
         "search-placeholder": "Tìm kiếm...",
 
+        // Trang chủ
+
+        // Về chúng tôi
+        "page-aboutus-title": "Về chúng tôi",
+
+        // Bài viết
         "page-blog-title": "Bài viết",
         "blog-1-title": "ELEPOCKET là cái gì vậy?",
         "blog-1-desc": "Bài viết đầu tiên của tụi mình!",
 
         "contact-label": "Liên hệ",
-        "footer-info-title": "Thông tin liên hệ"
+        "footer-info-title": "Thông tin liên hệ",
+
+        // Đăng ký - Đăng nhập
+        "sign-up-title": "Đăng ký",
+        "sign-in-title": "Đăng nhập",
+        "username-placeholder": "Tên người dùng",
+        "email-placeholder": "Email",
+        "password-placeholder": "Mật khẩu",
+        "agree": "Tôi đồng ý với",
+        "terms-of-service": "Điều khoản",
+        "and": "và",
+        "privacy-policy": "Chính sách bảo mật",
+        "of-ELE": "của Elepocket",
+        "sign-up-button": "Đăng ký",
+        "sign-in-button": "Đăng nhập",
+        "login-now": "Đăng nhập ngay",
+        "signup-now": "Đăng ký ngay",
+        "already-have-account": "Đã có tài khoản?",
+
+        "remember": "Ghi nhớ tôi",
+        "forgot-password": "Quên mật khẩu?",
+        "dont-have-account": "Chưa có tài khoản?",
     }
 }
 
