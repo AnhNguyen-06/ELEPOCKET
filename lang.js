af69c7a657314c0d96cf78403722383d2c035756
Anh Nguyen <731819nguyenvanquanganh@gmail.com>
2025-12-22 16:37:12 +0700

    update lang 2

--- lang.js
+++ lang.js
@@ -9,11 +9,38 @@ const translations = {
         "search-placeholder": "Search...",
         
         // Home
+        "slogan": "Smart Budget - Sparks&nbsp;Power",
+        "question-type-of-spending": "What is your type of spending?",
+        "get-started": "Get started",
 
         // About us
         "page-aboutus-title": "About us",
+        "description": "Smart ways to explore and manage your money",
+        "quote": "From little habits to big wins.",
 
         // Quick test
+        "page-quicktest-title": "Quick test",
+        "type-of-spending-test": "Type of spending test",
+        "free-test-for-students": "A free test about types of spending for students!",
+        "test-struct": "Test structure:",
+        "num-of-questions": "Number of questions: 9 - 10",
+        "duration": "Duration: 10 - 15 minutes",
+        "question-structure": "Question structure: 3 sections - income, spending, and saving",
+        "test-button": "Take the test now",
+
+        "about-the-test": "About the test",
+        "explanation": "This test is a tool that helps students measure their personal financial habits, categorized into three main levels: frugal spending, reasonable spending, uncontrolled spending.",
+
+        "why-take-the-test": "Why should you take the test?",
+        "explanation-why": "The spending level test not only helps users understand their own financial habits, but also identify issues such as overspending or financial stress, thereby enabling more reasonable and effective money management.",
+
+        "how-to-improve": "How to improve personal finance?",
+        "explanation-how-1": "Through our quick test, you can discover your current spending habits and receive practical advice to help adjust your financial behavior. With simple charts that illustrate spending and saving patterns,",
+        "explanation-how-2": "provides a clear overview of your personal finances and helps you find smarter ways to optimize your expenses.",
+
+        "discover-spending": "Discover your spending level",
+        "reveal-levels": "The test reveals three main levels:",
+        "three-levels": "Frugal spending → Reasonable spending → Wasteful spending",
 
         // Blog
         "page-blog-title": "Blog",
@@ -33,7 +60,7 @@ const translations = {
         "terms-of-service": "Terms of Service",
         "and": "and",
         "privacy-policy": "Privacy Policy",
-        "of-ELE": "of Elepocket",
+        "of-ELE": "of",
         "sign-up-button": "Sign up",
         "sign-in-button": "Sign in",
         "login-now": "Login now",
@@ -53,10 +80,39 @@ const translations = {
         "nav-signup": "Đăng ký",
         "search-placeholder": "Tìm kiếm...",
 
-        // Trang chủ
+        // Trang chủ   
+        "slogan": "Chi tiêu thông minh - Ví tiền rủng rỉnh",
+        "question-type-of-spending": "Mức độ chi tiêu của bạn là gì?",
+        "get-started": "Khám phá ngay",
 
         // Về chúng tôi
         "page-aboutus-title": "Về chúng tôi",
+        "description": "Chi tiêu thông minh - Ví tiền rủng rỉnh",
+        "quote": "Làm nên việc lớn từ những điều nhỏ nhất.",
+
+        // Test nhanh
+        "page-quicktest-title": "Test nhanh",
+        "type-of-spending-test": "Test mức độ chi tiêu",
+        "free-test-for-students": "Bài test mức độ chi tiêu dành cho học sinh hoàn toàn miễn phí!!",
+        "test-struct": "Cấu trúc bài test:",
+        "num-of-questions": "Số lượng câu hỏi: 9 - 10",
+        "duration": "Thời gian thực hiện: 10 - 15 phút",
+        "question-structure": "Cấu trúc câu hỏi: gồm 3 phần - thu nhập, chi tiêu và tiết kiệm",
+        "test-button": "Làm test ngay",
+
+        "about-the-test": "Bài test mức độ chi tiêu là gì?",
+        "explanation": "Bài test này là công cụ giúp học sinh đo lường tài chính bản thân, bao gồm các mức độ chính: chi tiêu tiết kiệm → chi tiêu hợp lý → chi tiêu thiếu kiểm soát.",
+
+        "why-take-the-test": "Vì sao bạn nên thực hiện bài test mức độ chi tiêu?",
+        "explanation-why": "Bài test mức độ chi tiêu không chỉ giúp người dùng hiểu rõ thói quen tài chính của bản thân, mà còn nhận biết vấn đề như lãng phí hoặc căng thẳng tiền bạc, từ đó giúp quản lý chi tiêu hợp lý và hiệu quả hơn.",
+
+        "how-to-improve": "Làm sao để cải thiện tài chính cá nhân?",
+        "explanation-how-1": "Qua kết quả của bài test nhanh, bạn sẽ khám phá được mức độ chi tiêu hiện tại, từ đó nhận được lời khuyên hữu ích để điểu chỉnh thói quen chi tiêu của mình. Bằng biểu đồ đơn giản biểu thị các mức chi tiêu và tiết kiệm,",
+        "explanation-how-2": "sẽ giúp bạn có cái nhìn tổng quan nhất về tình hình tài chính cá nhân, cũng như tìm ra cách tối ưu hóa chi tiêu của mình.",
+
+        "discover-spending": "Khám phá mức độ chi tiêu của bạn",
+        "reveal-levels": "Bài test cho biết các mức độ chính:",
+        "three-levels": "Chi tiêu tiết kiệm → Chi tiêu hợp lý → Chi tiêu lãng phí",
 
         // Bài viết
         "page-blog-title": "Bài viết",
@@ -76,7 +132,7 @@ const translations = {
         "terms-of-service": "Điều khoản",
         "and": "và",
         "privacy-policy": "Chính sách bảo mật",
-        "of-ELE": "của Elepocket",
+        "of-ELE": "của",
         "sign-up-button": "Đăng ký",
         "sign-in-button": "Đăng nhập",
         "login-now": "Đăng nhập ngay",
